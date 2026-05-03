be66710292d6c35b7136ac90a5942e18d33417af
arb-cs <arb_cs@protonmail.com>
2026-05-03 22:23:55 +0300

    Работа с авторизацией в Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -2,7 +2,8 @@ from playwright.sync_api import sync_playwright, expect
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context()
+    page = context.new_page()
 
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
@@ -22,3 +23,12 @@ with sync_playwright() as playwright:
 
     expect(dashboard_title).to_be_visible()
     expect(dashboard_title).to_have_text("Dashboard")
+
+    context.storage_state(path="browser-state.json")
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="browser-state.json")
+    page = context.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
